f1ce91d00451f29263719349361d2bbf05d3f1a7
bot <manaakumana51@gmail.com>
2026-07-28 07:29:34 +0700

    Update BOLA.js

--- BOLA.js
+++ BOLA.js
@@ -6,7 +6,7 @@
     const WIDGET_URL = "https://kdh-match.lovable.app";
 
     /* Tinggi widget yang terlihat */
-    const WIDGET_HEIGHT = 240;
+    const WIDGET_HEIGHT = 210;
 
     /* Tinggi asli iframe */
     const IFRAME_HEIGHT = 230;
